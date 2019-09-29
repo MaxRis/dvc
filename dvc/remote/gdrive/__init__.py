e67297a8c3954079864cb89e51160392adb462b6
Max Risuhin <risuhin.max@gmail.com>
2019-09-29 04:45:09 -0700

    Cleanup Gdrive client code

--- dvc/remote/gdrive/__init__.py
+++ dvc/remote/gdrive/__init__.py
@@ -17,7 +17,6 @@ from dvc.config import Config
 from dvc.remote.gdrive.utils import (
     TrackFileReadProgress,
     only_once,
-    metadata_isdir,
     shared_token_warning,
 )
 from dvc.exceptions import DvcException
@@ -34,63 +33,13 @@ class GDriveURLInfo(CloudURLInfo):
 
 
 class RemoteGDrive(RemoteBASE):
-    """Google Drive remote implementation
-
-    ## Some notes on Google Drive design
-
-    Google Drive identifies the resources by IDs instead of paths.
-
-    Folders are regular resources with an `application/vnd.google-apps.folder`
-    MIME type. Resource can have multiple parent folders, and also there could
-    be multiple resources with the same name linked to a single folder.
-
-    There are multiple root folders accessible from a single user account:
-    - `root` (special ID) - alias for the "My Drive" folder
-    - `appDataFolder` (special ID) - alias for the hidden application
-    space root folder
-    - shared drives root folders
-
-    ## Example URLs
-
-    - Datasets/my-dataset inside "My Drive" folder:
-
-        gdrive://root/Datasets/my-dataset
-
-    - Folder by ID (recommended):
-
-        gdrive://1r3UbnmS5B4-7YZPZmyqJuCxLVps1mASC
-
-        (get it https://drive.google.com/drive/folders/{here})
-
-    - Dataset named "my-dataset" in the hidden application folder:
-
-        gdrive://appDataFolder/my-dataset
-
-        (this one wouldn't be visible through Google Drive web UI and
-         couldn't be shared)
-    """
-
     scheme = Schemes.GDRIVE
     path_cls = GDriveURLInfo
     REGEX = r"^gdrive://.*$"
     REQUIRES = {"pydrive": GoogleAuth}
     PARAM_CHECKSUM = "md5Checksum"
-    DEFAULT_OAUTH_ID = "default"
-
-    # Default credential is needed to show the string of "Data Version
-    # Control" in OAuth dialog application name and icon in authorized
-    # applications list in Google account security settings. Also, the
-    # quota usage is limited by the application defined by client_id.
-    # The good practice would be to suggest the user to create their
-    # own application credentials.
-    DEFAULT_CREDENTIALPATH = os.path.join(
-        os.path.dirname(__file__), "google-dvc-client-id.json"
-    )
     GOOGLE_AUTH_SETTINGS_PATH = os.path.join(
         os.path.dirname(__file__), "settings.yaml"
-    )
-    SAVED_USER_CREDENTIALS_FILE = os.path.join(
-        os.path.dirname(__file__), "user-credentials"
     )
 
     def __init__(self, repo, config):
@@ -107,13 +56,6 @@ class RemoteGDrive(RemoteBASE):
         gauth = GoogleAuth(settings_file=self.GOOGLE_AUTH_SETTINGS_PATH)
         gauth.CommandLineAuth()
         return GoogleDrive(gauth)
-
-    def get_file_checksum(self, path_info):
-        raise DvcException("get_file_checksum my not impl", self.scheme)
-        file_id = self.get_path_id(path_info)
-        gdrive_file = self.gdrive.CreateFile({'id': file_id})
-        print("!!!!!Checksum:",gdrive_file['md5Checksum'])
-        return gdrive_file['md5Checksum']
 
     def cache_root_content(self):
         print("get_path_id %d" % self.root_content_cached)
@@ -138,12 +80,10 @@ class RemoteGDrive(RemoteBASE):
             parent_id = path_info.netloc
         file_list = self.gdrive.ListFile({'q': "'%s' in parents and trashed=false" % parent_id}).GetList()
 
-        #print("path parts", parts)
         for part in parts:
             file_id = ""
             for f in file_list:
                 if f['title'] == part:
-                    #print("Found path part:", part)
                     file_id = f['id']
                     file_list = self.gdrive.ListFile({'q': "'%s' in parents and trashed=false" % file_id}).GetList()
                     parent_id = f['id']
@@ -167,48 +107,6 @@ class RemoteGDrive(RemoteBASE):
             results.append(self.exists(path_info))
             callback.update(str(path_info))
         return results
-
-    def list_cache_paths(self):
-        raise DvcException("list_cache_paths my not impl", self.scheme)
-        try:
-            root = self.gdrive.get_metadata(self.path_info)
-        except GDriveResourceNotFound as e:
-            logger.debug("list_cache_paths: {}".format(e))
-        else:
-            prefix = self.path_info.path
-            for i in self.gdrive.list_children(root["id"]):
-                yield prefix + "/" + i
-
-    @only_once
-    def mkdir(self, parent, name):
-        raise DvcException("mkdir my not impl", self.scheme)
-        return self.gdrive.mkdir(parent, name)
-
-    def makedirs(self, path_info):
-        raise DvcException("makedirs my not impl", self.scheme)
-        parent = path_info.netloc
-        parts = iter(path_info.path.split("/"))
-        current_path = ["gdrive://" + path_info.netloc]
-        for part in parts:
-            try:
-                metadata = self.gdrive.get_metadata(
-                    self.path_cls.from_parts(
-                        self.scheme, parent, path="/" + part
-                    )
-                )
-            except GDriveResourceNotFound:
-                break
-            else:
-                current_path.append(part)
-                if not metadata_isdir(metadata):
-                    raise GDriveError(
-                        "{} is not a folder".format("/".join(current_path))
-                    )
-                parent = metadata["id"]
-        to_create = [part] + list(parts)
-        for part in to_create:
-            parent = self.mkdir(parent, part)["id"]
-        return parent
 
     def _upload(self, from_file, to_info, name, no_progress_bar):
         print("Upload %s %s %s" % (from_file, to_info, name))
